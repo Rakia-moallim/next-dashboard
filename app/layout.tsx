8c2a3c1fb01896f3c49a5b3a740a006de2ab0050
Refubz <rakia@warancloud.com>
2025-06-14 13:24:10 +0300

    all files were added

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+// importing Stylesheet file from the UI folder.
+import '@/app/ui/global.css';
+import {inter} from '@/app/ui/fonts'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
