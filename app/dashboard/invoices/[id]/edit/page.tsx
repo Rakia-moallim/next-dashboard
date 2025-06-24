b14a5bdfd9f4f4691cb67f8ddef1a87f8a57d9a9
Refubz <rakia@warancloud.com>
2025-06-24 13:16:06 +0300

    Added dynamic invoice detail page for specific invoice ID

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,27 @@
+import { customers, invoices } from "@/app/lib/placeholder-data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+// import Form from "@/app/ui/invoices/create-form";
+import Form from "@/app/ui/invoices/edit-form"
+import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
+
+export default async function page(props: {params: Promise<{id: string}>}){
+     
+    const params = await props.params;
+    const id = params.id;
+    const[invoice, customers] = await Promise.all([
+        fetchInvoiceById(id),
+        fetchCustomers(),
+    ])
+
+    return(
+        <main>
+            <Breadcrumbs breadcrumbs={[
+                {label: 'Invoice', href: '/dashboard/invoices'},
+                {label: 'Edit Invoice', href: '/dashboard/invoics/${id}/edit', active: true,},  
+            ]}/>
+            {/* {<Form invoice={invoices} customers={customers}/> } */}
+
+            <Form invoice={invoice} customers={customers}/>
+        </main>
+    );
+}
