c4e222375d800393f80dad2052c93958d1b8464b
Rakia Moallim <138783159+Rakia-moallim@users.noreply.github.com>
2025-06-25 14:57:05 +0300

    Fix Next.js export type error in invoices page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -2,47 +2,45 @@
 //   return <p>Invoices Dashboard Page</p>;
 // }
 
-
 import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
-import Table from '@/app/ui/invoices/table'
+import Table from "@/app/ui/invoices/table";
 import { lusitana } from "@/app/ui/fonts";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { Suspense } from "react";
 import { InvoiceSkeleton } from "@/app/ui/skeletons";
 import { fetchInvoicesPages } from "@/app/lib/data";
 
- 
-export default async function Page(props:{
-  SearchParams?: Promise<{
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
     query?: string;
     page?: string;
-  }>;
-}){
+  };
+}) {
+  // const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
 
-    const searchParams = await props.SearchParams;
-    const query = searchParams?.query || '';
-    const currentPage = Number(searchParams?.page) || 1;
-    const totalPages = await fetchInvoicesPages(query);
-
-    return(
-      <div className="w-full">
+  return (
+    <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2x1`}>Invoices</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search invoices.."/>
-        <CreateInvoice/>
+        <Search placeholder="Search invoices.." />
+        <CreateInvoice />
       </div>
-      
-      <Suspense key={query + currentPage} fallback={<InvoiceSkeleton/>}>
-        <Table query={query} currentPage={currentPage}/>
+
+      <Suspense key={query + currentPage} fallback={<InvoiceSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
       </Suspense>
-      
+
       <div className="mt-5 flex justify-center">
         <Pagination totalPages={totalPages} />
       </div>
-      </div>
-    );
-
+    </div>
+  );
 }
